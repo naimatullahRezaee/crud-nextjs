7d6c6143c2eda433b60d2153d0317fa923151abf
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-06-05 11:02:31 +0200

    fist text add to the page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text">CRUD APP</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient">This App Is For Testing</span>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam
+        quibusdam ducimus dignissimos sequi laboriosam dolorum voluptates
+        inventore molestias et, consequatur quo neque esse, ex quod eum
+        provident! Cupiditate, explicabo omnis.
+      </p>
+      {/* Feed  */}
+    </section>
   );
 };
 
