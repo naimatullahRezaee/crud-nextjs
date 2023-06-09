3c0e04a12a183058e5c16685b979c648a9eea65e
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-06-09 08:14:17 +0200

    google authentication is add

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
@@ -9,10 +11,38 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
-    } catch (error) {}
+      await connectToDB();
+
+      //  check if  a user already exists
+
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      // if not, create a new user
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 export { handler as GET, handler as POST };
